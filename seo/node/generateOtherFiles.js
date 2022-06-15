eef30cf947afb22d2b9821bfd44c07250da1dd6c
Kurt Lourens <hi@kurtlourens.com>
2022-06-15 12:11:03 +0200

    regen seo

--- seo/node/generateOtherFiles.js
+++ seo/node/generateOtherFiles.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const util = require('util');
 const Handlebars = require('handlebars');
 
+const pjson = require('../../package.json');
+
 const dateHelper = require('../handlebar/helpers/date.helper.js');
 const loudHelper = require('../handlebar/helpers/loud.helper.js');
 const urlrefHelper = require('../handlebar/helpers/urlref.helper.js');
@@ -10,30 +12,35 @@ const versionHelper = require('../handlebar/helpers/version.helper.js');
 const readFile = util.promisify(fs.readFile);
 
 async function generateOtherFiles() {
-    const projectDataContents = await readFile('./data/project.json', 'utf8');
-    const projectData = JSON.parse(projectDataContents);
-
+    process.env['NODE_ENV'] = pjson.version;
     Handlebars.registerHelper('date', dateHelper);
     Handlebars.registerHelper('loud', loudHelper);
     Handlebars.registerHelper('urlref', urlrefHelper);
     Handlebars.registerHelper('version', versionHelper);
+    
+    const projectDataContents = await readFile('./data/project.json', 'utf8');
+    const projectData = JSON.parse(projectDataContents);
 
+    const publicFolder = '../public/';
     const files = [
-        'humans.txt',
-        'opensearch.xml',
-        'sitemap.xml',
-        'web.config',
+        { template: './handlebar/index.html.hbs', dest: '../index.html'},
+        { template: './handlebar/humans.txt.hbs', dest: publicFolder + 'humans.txt'},
+        { template: './handlebar/opensearch.xml.hbs', dest: publicFolder + 'opensearch.xml'},
+        { template: './handlebar/manifest.json.hbs', dest: publicFolder + 'manifest.json'},
+        { template: './handlebar/site.webmanifest.hbs', dest: publicFolder + 'site.webmanifest'},
+        { template: './handlebar/sitemap.xml.hbs', dest: publicFolder + 'sitemap.xml'},
+        { template: './handlebar/web.config.hbs', dest: publicFolder + 'web.config'},
     ]
 
-    for (const file of files) {
-        const template = await readFile(`./handlebar/${file}.hbs`, 'utf8');
+    for (const fileObj of files) {
+        const template = await readFile(fileObj.template, 'utf8');
         const templateFunc = Handlebars.compile(template);
         const templateData = {
             ...projectData,
             allItems: []
         };
         const compiledTemplate = templateFunc(templateData);
-        fs.writeFile(`../public/${file}`, compiledTemplate, ['utf8'], () => { });
+        fs.writeFile(fileObj.dest, compiledTemplate, ['utf8'], () => { });
     }
 }
 
---
+++ seo/node/getAirtableData.js
@@ -0,0 +1,114 @@
+const fs = require('fs');
+const util = require('util');
+const Airtable = require('airtable');
+
+require('dotenv').config();
+
+const tables = {
+    Main: {
+        name: 'Main',
+        view: 'VisibleCommunities',
+        json: 'communityList.json',
+        records: [
+            {
+                source: 'Name',
+                target: 'name',
+            },
+            {
+                source: 'Icon',
+                target: 'icon',
+                mapping: (v) => v?.[0]?.url ?? '',
+            },
+            {
+                source: 'Banners',
+                target: 'banners',
+                mapping: (v) => v?.flatMap(inner => (inner?.url ?? '').split(',')).filter(item => item?.length > 0),
+            },
+            {
+                source: 'Description',
+                target: 'desc',
+            },
+            {
+                source: 'Grouping',
+                target: 'group',
+                mapping: (v) => v?.[0],
+            },
+            {
+                source: 'Tags',
+                target: 'tags',
+            },
+            {
+                source: 'Links',
+                target: 'links',
+                mapping: (v) => v.split(',').filter(item => item?.length > 0),
+            },
+            {
+                source: 'Sort',
+                target: 'sort',
+            }
+        ]
+    },
+    // Group: {
+    //     name: 'Group',
+    //     view: 'AvailableGroups',
+    //     json: 'communityLookup.json',
+    //     records: [
+    //         {
+    //             source: 'Name',
+    //             target: 'name',
+    //         },
+    //         {
+    //             source: 'Icon',
+    //             target: 'icon',
+    //             mapping: (v) => v?.[0]?.url ?? '',
+    //         },
+    //     ],
+    // },
+};
+
+async function generateJsonFromAirtable() {
+    const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appW3eQpG1MlolQ25');
+
+    const tableKeys = Object.keys(tables);
+    for (let tableIndex = 0; tableIndex < tableKeys.length; tableIndex++) {
+        const tableKey = tableKeys[tableIndex];
+        const currentTable = tables[tableKey];
+
+        const allItems = [];
+        base(currentTable.name).select({
+            view: currentTable.view
+        }).eachPage(function page(records, fetchNextPage) {
+
+            records.forEach(function (record) {
+                let obj = {
+                    id: record.id,
+                };
+                for (const rec of currentTable?.records ?? []) {
+                    let value;
+                    if (rec.mapping != null) {
+                        value = rec.mapping(record.get(rec.source));
+                    } else {
+                        value = record.get(rec.source);
+                    }
+                    try {
+                        obj[rec.target] = value;
+                    } catch (ex) {
+                        console.log(ex);
+                    }
+                }
+                allItems.push(obj);
+            });
+            fetchNextPage();
+        }, function done(err) {
+            if (err) {
+                console.error({ err });
+                return;
+            }
+            console.log('write file ' + allItems.length);
+            fs.writeFile(`../src/assets/data/${currentTable.json}`, JSON.stringify(allItems), ['utf8'], () => { });
+        });
+    }
+}
+
+
+generateJsonFromAirtable();
